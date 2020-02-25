4a5004fb8868f74479b795ca192d14ab2abfdad2
mbrodniak <47107265+mbrodniak@users.noreply.github.com>
2020-02-25 10:36:10 +0100

    v1

---
+++ src/Data/Movie.ts
@@ -0,0 +1,15 @@
+export class Movie {
+
+  imdbId: string;
+  ratings: number;
+  votesNumber: number;
+  titleType: string;
+  primaryTitle: string;
+  originalTitle: string;
+  startYear: number;
+  runtimeMinutes: number;
+  genere1: string;
+  genere2: string;
+  adult: boolean;
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ngx-bootstrap';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SelectionComponent } from './selection/selection.component';
+import {DataService} from './data.service';
+import { HttpClientModule} from '@angular/common/http';
+
+const appRoutes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  },
+  {
+    path: 'selection', component: SelectionComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SelectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
+    AlertModule.forRoot(),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
